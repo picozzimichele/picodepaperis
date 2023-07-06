68156c86ce6da70b73d5b4bb012e332b73dca21e
Michele Picozzi <michele.picozzi@hotmail.it>
2023-07-06 17:51:12 +0800

    added photo slider

--- app/photography/page.tsx
+++ app/photography/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function Photography() {
     return (
-        <div className="flex w-full bg-green-200">
+        <div className="flex w-full bg-green-200 h-screen overflow-hidden">
             <div className="flex flex-col w-full">
                 <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
                     <PhotoColumn />
--- components/photography-card/PhotoColumn.tsx
+++ components/photography-card/PhotoColumn.tsx
@@ -3,14 +3,25 @@ import PhotoCard from "./PhotoCard";
 
 export default function PhotoColumn() {
     return (
-        <div className="flex flex-col gap-4">
-            <PhotoCard />
-            <PhotoCard />
-            <PhotoCard />
-            <PhotoCard />
-            <PhotoCard />
-            <PhotoCard />
-            <PhotoCard />
+        <div className="flex flex-col group gap-4">
+            <div className="flex flex-col gap-4 group-hover:animationpaused itemslide">
+                <PhotoCard />
+                <PhotoCard />
+                <PhotoCard />
+                <PhotoCard />
+                <PhotoCard />
+                <PhotoCard />
+                <PhotoCard />
+            </div>
+            <div className="flex flex-col gap-4 group-hover:animationpaused itemslide">
+                <PhotoCard />
+                <PhotoCard />
+                <PhotoCard />
+                <PhotoCard />
+                <PhotoCard />
+                <PhotoCard />
+                <PhotoCard />
+            </div>
         </div>
     );
 }
