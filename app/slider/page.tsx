d9aa1d68ecc3e6642b3fb5f559aa4a40aea4163c
Michele Picozzi <michele.picozzi@hotmail.it>
2024-08-09 18:27:33 +0800

    slider CSS

--- app/slider/page.tsx
+++ app/slider/page.tsx
@@ -1,5 +1,60 @@
 import React from "react";
+import Image from "next/image";
 
 export default function Page() {
-    return <div>P</div>;
+    return (
+        <div className="bannerMain">
+            <div className="bannerSlider" style={{ "--quantity": 4 } as React.CSSProperties}>
+                <div className="bannerItem" style={{ "--position": 1 } as React.CSSProperties}>
+                    <div className="h-full w-full relative overflow-hidden rounded-lg">
+                        <Image
+                            className="object-cover"
+                            src="/images/projects/goatgaming.png"
+                            fill={true}
+                            alt="Slider Screenshot"
+                        />
+                    </div>
+                </div>
+                <div
+                    className="bannerItem absolute inset-0"
+                    style={{ "--position": 2 } as React.CSSProperties}
+                >
+                    <div className="h-full w-full relative overflow-hidden rounded-lg">
+                        <Image
+                            className="object-cover"
+                            src="/images/projects/survivors.png"
+                            fill={true}
+                            alt="Slider Screenshot"
+                        />
+                    </div>
+                </div>
+                <div
+                    className="bannerItem absolute inset-0"
+                    style={{ "--position": 3 } as React.CSSProperties}
+                >
+                    <div className="h-full w-full relative overflow-hidden rounded-lg">
+                        <Image
+                            className="object-cover"
+                            src="/images/projects/tilevania.png"
+                            fill={true}
+                            alt="Slider Screenshot"
+                        />
+                    </div>
+                </div>
+                <div
+                    className="bannerItem absolute inset-0"
+                    style={{ "--position": 4 } as React.CSSProperties}
+                >
+                    <div className="h-full w-full relative overflow-hidden rounded-lg">
+                        <Image
+                            className="object-cover"
+                            src="/images/projects/miroir.png"
+                            fill={true}
+                            alt="Slider Screenshot"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
